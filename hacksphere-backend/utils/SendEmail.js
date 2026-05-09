d9798bd905276d1c6779c42947a054e2da31a900
Sanafarhat <sanafarhat005@gmail.com>
2026-05-09 14:19:47 +0530

    sendig email update

--- hacksphere-backend/utils/SendEmail.js
+++ hacksphere-backend/utils/SendEmail.js
@@ -9,6 +9,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  family: 4, // ← force IPv4
 });
 
 // Verify transporter on startup
